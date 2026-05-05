8b65453009f0bf6413d14fbfc0b1e2bbcb0e92b4
Henry Mascot <henrino3@gmail.com>
2026-05-05 06:28:53 +0100

    chore: remove local development clutter from root

--- commit-context.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/henrymascot/Code/entity
-git add -A .context/ docs/ memory/ README.md scripts/update-context.sh
-git status
-git commit -m "docs: comprehensive context update reflecting Feb 14-17 work"
-echo "Done!"
